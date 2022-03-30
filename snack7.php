33639ae0f54a373519d7bd4a47f1e77fe3f4e70c
AndreaGir <girolamiandrea2000@gmail.com>
2022-03-30 11:06:25 +0200

    snack 7

--- snack7.php
+++ snack7.php
@@ -1,5 +1,23 @@
 <?php
 
+$classe = [
+    [
+        'nome' => 'mimmo',
+        'cognome'=> 'roberto',
+        'voti' => [31,22,28,34]
+    ],
+    [
+        'nome' => 'franco',
+        'cognome'=> 'gigini',
+        'voti' => [30,22,58,27]
+    ],
+    [
+        'nome' => 'andrea',
+        'cognome'=> 'giro',
+        'voti' => [30,29,28,32]
+    ],
+]
+
 
 ?>
 
@@ -13,6 +31,23 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+
+for($i= 0 ; $i < count($classe); $i++){
+    $studente = $classe[$i];
+    $voti = $studente['voti'];
+?>
+<p>
+    <?php
+    echo $studente['nome'] . ' ' .$studente['cognome']; array_sum($voti)/count($voti)
+    ?>
+</p>
+   <?php 
+}
+
+
+?>
     
 </body>
 </html>
